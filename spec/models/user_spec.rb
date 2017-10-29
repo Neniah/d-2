eee70866a37b1d36c1dbf68295ee336c67b65358
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-10-29 17:07:40 +0100

    adding devise gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with a first name, last name, email, and password"
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: 'Maria',
+      last_name: 'Lobillo',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavillion-tights-furze',
+    )
+    expect(user).to be_valid
+  end
   it "is invalid without a fisrt name"
   it "is invalid without a last name"
   it 'is invalid without an email address'
