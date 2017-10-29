20fe5f1fd890da4d9d2ab6578ae85ddfd90fa241
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-10-29 18:13:09 +0100

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # validations
+  validates :last_name, presence: true
+  validates :email, uniqueness: true, presence: true
+
+  def full_name
+    :first_name + ' ' + :last_name
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,45 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
-  it "is invalid without a fisrt name"
-  it "is invalid without a last name"
-  it 'is invalid without an email address'
-  it 'is invalid with a duplicate email address'
-  it 'returns a users full name as a string'
+
+  it "is invalid without a fisrt name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to_not include("can't be blank")
+  end
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+  it 'is invalid without an email address' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+  it 'is invalid with a duplicate email address' do
+    User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-pavillion'
+    )
+    user = User.new(
+      first_name: 'Jane',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-pavillion'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
+  end
+  xit 'returns a users full name as a string' do
+    user = User.new(
+      first_name: 'Jane',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-pavillion',
+    )
+
+  end
 end
